94389828ff03d77f9ec78597ffe3a3644d7b5d4d
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-24 16:07:38 +0900

    Initial commit

---
+++ src/main/java/com/apploidxxx/heliosbackend/HeliosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.apploidxxx.heliosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeliosBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apploidxxx/heliosbackend/HeliosBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apploidxxx.heliosbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeliosBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
