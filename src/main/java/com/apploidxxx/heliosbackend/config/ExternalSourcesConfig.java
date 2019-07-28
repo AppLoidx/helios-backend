5cea401677430b99b1a6e0b8eafe40b385cb094a
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-28 17:30:28 +0900

    added external source configuration static class

---
+++ src/main/java/com/apploidxxx/heliosbackend/config/ExternalSourcesConfig.java
@@ -0,0 +1,8 @@
+package com.apploidxxx.heliosbackend.config;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+public class ExternalSourcesConfig {
+    public static final String heliosApiUri = "http://localhost:3000/api/";
+}
