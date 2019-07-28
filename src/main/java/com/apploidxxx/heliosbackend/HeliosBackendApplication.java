875c7e567cc6f099c4a3f90d24a193c9f7eac6bb
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-28 17:39:00 +0900

    added additional server configurations

--- src/main/java/com/apploidxxx/heliosbackend/HeliosBackendApplication.java
+++ src/main/java/com/apploidxxx/heliosbackend/HeliosBackendApplication.java
@@ -2,8 +2,11 @@ package com.apploidxxx.heliosbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableJpaRepositories("com.apploidxxx.heliosbackend.data.repository")
 public class HeliosBackendApplication {
 
     public static void main(String[] args) {
