0c8a0c4a98c96836a19bcd78b91d91700d02a19c
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-06 18:27:27 +0900

    added toString lombok annotation

--- src/main/java/com/apploidxxx/heliosbackend/rest/model/ErrorMessage.java
+++ src/main/java/com/apploidxxx/heliosbackend/rest/model/ErrorMessage.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 /**
  * @author Arthur Kupriyanov
  */
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class ErrorMessage {
     @JsonAlias("error")
     public String error;
