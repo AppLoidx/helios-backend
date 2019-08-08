9ff6b8a679d76389ad557cd780aaf1534d0891fb
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-08 20:23:57 +0900

    wrapped to MD5.crypt (hashing method)

--- src/main/java/com/apploidxxx/heliosbackend/rest/util/SessionGenerator.java
+++ src/main/java/com/apploidxxx/heliosbackend/rest/util/SessionGenerator.java
@@ -1,14 +1,15 @@
 package com.apploidxxx.heliosbackend.rest.util;
 
+import org.apache.commons.codec.digest.Md5Crypt;
+
 import java.util.Base64;
+import java.util.Date;
 
 /**
  * @author Arthur Kupriyanov
  */
 public class SessionGenerator {
     public static String generateSession(String login, String password) {
-        // todo: write correct body to session generator
-
-        return Base64.getEncoder().encodeToString((login + password).getBytes());
+        return Base64.getEncoder().encodeToString(Md5Crypt.md5Crypt((login + new Date().toString() + password).getBytes()).getBytes());
     }
 }
