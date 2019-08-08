68b00e33466804a73113a903baef0aab67408537
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-08 20:23:09 +0900

    added session control

--- src/main/java/com/apploidxxx/heliosbackend/rest/OAuthRestController.java
+++ src/main/java/com/apploidxxx/heliosbackend/rest/OAuthRestController.java
@@ -11,7 +11,10 @@ import com.apploidxxx.heliosbackend.rest.util.SessionGenerator;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.Optional;
 
 
@@ -29,10 +32,10 @@ public class OAuthRestController {
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
-    Object getToken(HttpServletResponse response,
+    Object getToken(HttpServletResponse response, HttpServletRequest request,
                     @CookieValue(value = "session", defaultValue = "null") String session,
                     @RequestParam(value = "login", defaultValue = "") String login,
-                    @RequestParam(value = "password", defaultValue = "") String password) {
+                    @RequestParam(value = "password", defaultValue = "") String password) throws IOException {
 
         if (login.equals("") || password.equals("")) {
             if (session.equals("null")) {
@@ -41,12 +44,18 @@ public class OAuthRestController {
             } else {
                 Optional<User> optUser = userRepository.findBySession(session);
                 if (optUser.isPresent()) {
+                    HttpSession oldSession = request.getSession(false);
+                    if (oldSession != null) {
+                        oldSession.invalidate();
+                    }
+                    HttpSession newSession = request.getSession(true);
                     response.setStatus(HttpServletResponse.SC_OK);
                     // todo : add authorization to security
+                    response.sendRedirect("helios.html");
                     return null;
                 } else {
                     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                    return new ErrorMessage("invalid session", "Your session token is invalid. Please update your session token with new request that has GET parameters for auth");
+                    return new ErrorMessage("invalid_session", "Your session token is invalid. Please update your session token with new request that has GET parameters for auth");
                 }
             }
 
@@ -58,6 +67,7 @@ public class OAuthRestController {
                 response.addCookie(new Cookie("session", sessionId));
                 response.setStatus(HttpServletResponse.SC_OK);
                 // todo : add authorization to security
+                response.sendRedirect("helios.html");
                 return null;
 
             } catch (UnauthorizedException e) {
