36b1e4ef959f5ffa05a76319478a7066138df437
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-09 18:14:09 +0900

    added Queue Rest Controller

---
+++ src/main/java/com/apploidxxx/heliosbackend/rest/QueueRestController.java
@@ -0,0 +1,93 @@
+package com.apploidxxx.heliosbackend.rest;
+
+import com.apploidxxx.heliosbackend.data.entity.User;
+import com.apploidxxx.heliosbackend.data.repository.UserRepository;
+import com.apploidxxx.heliosbackend.rest.exceptions.UserNotFoundException;
+import com.apploidxxx.heliosbackend.rest.model.Queue;
+import com.apploidxxx.heliosbackend.rest.util.Request;
+import com.apploidxxx.heliosbackend.rest.util.UserManager;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+@RestController
+@RequestMapping("/api/queue")
+public class QueueRestController {
+    private final UserRepository userRepository;
+
+    public QueueRestController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody
+    Object
+    get(HttpServletResponse response, @CookieValue("session") String session) throws IOException {
+        User user;
+        try {
+            user = new UserManager(userRepository).getUser(session);
+        } catch (UserNotFoundException e) {
+            return e.wrapResponse(response);
+        }
+
+        ResponseEntity<Queue> queueResponseEntity = new Request().get("queue", Queue.class, "access_token", user.getUserToken().getAccessToken());
+
+        response.setStatus(queueResponseEntity.getStatusCode().value());
+
+        return queueResponseEntity.getBody();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, produces = "application/json")
+    public @ResponseBody
+    Object
+    put(HttpServletResponse response, @CookieValue("session") String session,
+        @RequestParam(value = "queue_name") String queueName,
+        @RequestParam(value = "password", defaultValue = "") String password) {
+
+        try {
+            Request.put("queue", null,
+                    "access_token", new UserManager(userRepository).getUser(session).getUserToken().getAccessToken(),
+                    "queue_name", queueName,
+                    "password", password);
+        } catch (UserNotFoundException e) {
+            return e.wrapResponse(response);
+        } catch (HttpStatusCodeException e) {
+            response.setStatus(e.getStatusCode().value());
+            return e.getResponseBodyAsString();
+        }
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public @ResponseBody
+    Object
+    post(HttpServletResponse response, @CookieValue("session") String session,
+         @RequestParam("queue_name") String queueName,
+         @RequestParam("fullname") String fullname,
+         @RequestParam("generation") String genaration,
+         @RequestParam("password") String password) {
+
+        try {
+            Request.put("queue", null,
+                    "access_token", new UserManager(userRepository).getUser(session).getUserToken().getAccessToken(),
+                    "queue_name", queueName,
+                    "password", password,
+                    "fullname", fullname,
+                    "generation", genaration);
+        } catch (UserNotFoundException e) {
+            return e.wrapResponse(response);
+        } catch (HttpStatusCodeException e) {
+            response.setStatus(e.getStatusCode().value());
+            return e.getResponseBodyAsString();
+        }
+        return null;
+    }
+
+
+}
