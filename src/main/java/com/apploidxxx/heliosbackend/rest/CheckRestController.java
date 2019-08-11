3c99951d8101d84ddeb174ee395293892d822dfd
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-11 10:43:15 +0900

    added check rest controller

---
+++ src/main/java/com/apploidxxx/heliosbackend/rest/CheckRestController.java
@@ -0,0 +1,25 @@
+package com.apploidxxx.heliosbackend.rest;
+
+import com.apploidxxx.heliosbackend.rest.util.Request;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+@RestController
+@RequestMapping("/api/check")
+public class CheckRestController {
+    @GetMapping(produces = "application/json")
+    public @ResponseBody
+    Object get(@RequestParam("check") String check,
+               @RequestParam(value = "queue_name", defaultValue = "") String queueName,
+               @RequestParam(value = "username", defaultValue = "") String username) {
+        try {
+            return new Request().get("check", String.class, "check", check,
+                    "queue_name", queueName, "username", username);
+        } catch (HttpStatusCodeException e) {
+            return e.getResponseBodyAsString();
+        }
+    }
+}
