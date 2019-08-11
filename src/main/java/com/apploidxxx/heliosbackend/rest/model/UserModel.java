0231e783679aeb52fde1ebebd157485cd460fc68
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-11 10:42:23 +0900

    added queues member property

--- src/main/java/com/apploidxxx/heliosbackend/rest/model/UserModel.java
+++ src/main/java/com/apploidxxx/heliosbackend/rest/model/UserModel.java
@@ -1,12 +1,17 @@
 package com.apploidxxx.heliosbackend.rest.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
+import java.util.List;
 
 /**
  * @author Arthur Kupriyanov
  */
 @Data
 public class UserModel {
-    private String[] queues;
+    private List<String[]> queues;
+    @JsonProperty("queues_member")
+    private List<String[]> queuesMember;
     private User user;
 }
