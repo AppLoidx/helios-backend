cacc323d7fdc1ac6279d5dc57b51ce8926d43c27
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-13 12:01:08 +0900

    added error handler controller

---
+++ src/main/java/com/apploidxxx/heliosbackend/handlers/ErrorController.java
@@ -0,0 +1,38 @@
+package com.apploidxxx.heliosbackend.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404.html";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500.html";
+            }
+        }
+        return "error";
+    }
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
