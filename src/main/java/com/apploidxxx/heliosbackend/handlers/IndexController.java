658439fa8f77c37b70451a6c4907e59914d73501
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-13 12:01:28 +0900

    added index controller for index.html

---
+++ src/main/java/com/apploidxxx/heliosbackend/handlers/IndexController.java
@@ -0,0 +1,29 @@
+package com.apploidxxx.heliosbackend.handlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String mainPage(){
+        return "index.html";
+    }
+    @RequestMapping("")
+    public String main(){
+        return "index.html";
+    }
+
+    @RequestMapping("/policy")
+    public String policyPage(){
+        return "policy/index.html";
+    }
+
+    @RequestMapping("/policy/")
+    public String policy(){
+        return "index.html";
+    }
+}
