e34577531d376667f025d8520efed00facac2717
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-17 21:13:28 +0900

    configured cache control

--- src/main/java/com/apploidxxx/heliosbackend/config/StaticResourceConfig.java
+++ src/main/java/com/apploidxxx/heliosbackend/config/StaticResourceConfig.java
@@ -1,6 +1,7 @@
 package com.apploidxxx.heliosbackend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,12 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class StaticResourceConfig implements WebMvcConfigurer {
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/META-INF/resources/","classpath:/resources/static/",
             "classpath:/static/", "classpath:/public/"};
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+        registry.addResourceHandler("/resources/static/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS)
+        .setCacheControl(CacheControl.noCache())
+        .setCachePeriod(0);
     }
 }
