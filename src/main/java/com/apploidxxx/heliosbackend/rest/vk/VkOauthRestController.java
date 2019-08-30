11a6dfae8e29958ac4c8257687f114818d93c96c
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-31 04:05:38 +0900

    VK OAuth new methods

--- src/main/java/com/apploidxxx/heliosbackend/rest/vk/VkOauthRestController.java
+++ src/main/java/com/apploidxxx/heliosbackend/rest/vk/VkOauthRestController.java
@@ -4,6 +4,7 @@ import com.apploidxxx.heliosbackend.data.entity.User;
 import com.apploidxxx.heliosbackend.data.repository.UserRepository;
 import com.apploidxxx.heliosbackend.rest.exceptions.UserNotFoundException;
 import com.apploidxxx.heliosbackend.rest.network.api.vk.OAuthVK;
+import com.apploidxxx.heliosbackend.rest.util.SessionGenerator;
 import com.apploidxxx.heliosbackend.rest.util.UserManager;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +15,7 @@ import java.io.IOException;
  * @author Arthur Kupriyanov
  */
 @RestController
-@RequestMapping("api/vk/link")
+@RequestMapping("api/vk/oauth")
 public class VkOauthRestController {
     private final UserRepository userRepository;
 
@@ -23,6 +24,7 @@ public class VkOauthRestController {
     }
 
     @GetMapping
+    @RequestMapping("/link")
     public @ResponseBody
     Object get(@CookieValue("session") String session,
                HttpServletResponse response) throws IOException {
@@ -33,6 +35,16 @@ public class VkOauthRestController {
             return e.wrapResponse(response);
         }
         response.sendRedirect(OAuthVK.getCodeTokenPath(String.valueOf(user.getId())));
+        return null;
+    }
+
+    @GetMapping
+    @RequestMapping("/register")
+    public @ResponseBody
+    Object getRegisterMap(HttpServletResponse response) throws IOException {
+        String session = SessionGenerator.generateSession("vk", "oauth");
+
+        response.sendRedirect(OAuthVK.getCodeTokenPath(session));
         return null;
     }
 
