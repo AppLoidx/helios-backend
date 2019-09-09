58eba3720dc3d0197238466bff2c6cd01fb55aac
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-09-10 00:42:49 +0300

    created UserSocialDataRepository

---
+++ src/main/java/com/apploidxxx/heliosbackend/data/repository/UserSocialDataRepository.java
@@ -0,0 +1,15 @@
+package com.apploidxxx.heliosbackend.data.repository;
+
+import com.apploidxxx.heliosbackend.data.entity.UserSocialData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * @author Arthur Kupriyanov
+ */
+public interface UserSocialDataRepository extends JpaRepository<UserSocialData, Long> {
+    @Override
+    Optional<UserSocialData> findById(Long aLong);
+    Optional<UserSocialData> findByEmail(String email);
+}
