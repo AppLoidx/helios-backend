f519977f20e4ee34802b242e30b4d4f7c5c1d5a9
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-09-27 01:53:42 +0300

    rewritten for OAuth 2.0 and code refactoring

--- src/main/java/com/apploidxxx/heliosbackend/rest/OAuthRestController.java
+++ src/main/java/com/apploidxxx/heliosbackend/rest/OAuthRestController.java
@@ -5,25 +5,25 @@ import com.apploidxxx.heliosbackend.data.entity.Token;
 import com.apploidxxx.heliosbackend.data.entity.User;
 import com.apploidxxx.heliosbackend.data.repository.UserRepository;
 import com.apploidxxx.heliosbackend.rest.model.ErrorMessage;
-import com.apploidxxx.heliosbackend.rest.network.api.helios.OAuthAuthorize;
-import com.apploidxxx.heliosbackend.rest.network.api.helios.exception.UnauthorizedException;
+import com.apploidxxx.heliosbackend.rest.model.UserModel;
 import com.apploidxxx.heliosbackend.rest.util.SessionGenerator;
-import org.hibernate.TransientObjectException;
+import com.apploidxxx.heliosbackend.rest.util.UserRestGetter;
+import com.apploidxxx.heliosbackend.rest.util.request.Request;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Optional;
 
 
 /**
  * @author Arthur Kupriyanov
  */
 @RestController
-@RequestMapping(value = "/api/auth")
+@RequestMapping(value = "/api/oauth")
 public class OAuthRestController {
     private final UserRepository userRepository;
 
@@ -34,68 +34,83 @@ public class OAuthRestController {
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
     Object getToken(HttpServletResponse response, HttpServletRequest request,
-                    @CookieValue(value = "session", defaultValue = "null") String session,
-                    @RequestParam(value = "login", defaultValue = "") String login,
-                    @RequestParam(value = "password", defaultValue = "") String password) throws IOException {
+                    @RequestParam(value = "authorization_code", defaultValue = "") String authorizationCode) {
 
-        if (login.equals("") || password.equals("")) {
-            if (session.equals("null")) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return new ErrorMessage("invalid login or password", "Your login or password is invalid. Please, add the GET parameters on your http request");
-            } else {
-                Optional<User> optUser = userRepository.findBySession(session);
-                if (optUser.isPresent()) {
-                    HttpSession oldSession = request.getSession(false);
-                    if (oldSession != null) {
-                        oldSession.invalidate();
-                    }
-                    request.getSession(true);
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    response.sendRedirect("/helios.html");
-                    return null;
-                } else {
-                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                    return new ErrorMessage("invalid_session", "Your session token is invalid. Please update your session token with new request that has GET parameters for auth");
-                }
-            }
-
-        } else {
-            try {
-                Token token = OAuthAuthorize.getTokens(login, password);                // авторизация с helios - api oauth
-                Optional<User> userOpt;
-                try {
-                    userOpt = userRepository.findByUsername(login);
-                    String sessionId = SessionGenerator.generateSession(login, password);
-                    if (userOpt.isPresent()) {
-                        userOpt.get().setSession(sessionId);
-                        userOpt.get().getUserToken().setAccessToken(token.getAccessToken());
-                        userOpt.get().getUserToken().setRefreshToken(token.getRefreshToken());
-                        userOpt.get().setEmailFromAPI();
-                        response.addCookie(new Cookie("session", userOpt.get().getSession()));
-                        userRepository.save(userOpt.get());
-                    } else {
-                        userRepository.save(new User(token, sessionId, login));
-                        response.addCookie(new Cookie("session", sessionId));
-                    }
-                } catch (TransientObjectException e) {
-                    System.err.println(e.getMessage());
-                    String sessionId = SessionGenerator.generateSession(login, password);
-                    userRepository.save(new User(token, sessionId, login));
-                    response.addCookie(new Cookie("session", sessionId));
-                }
-                request.getSession(true);
-
-
-                response.setStatus(HttpServletResponse.SC_OK);
-                response.sendRedirect("/helios.html");
-
-                return null;
-
-            } catch (UnauthorizedException e) {
-                response.setStatus(e.getStatus());
-                return e.getErrMsg();
-            }
+        Token token;
+        try {
+            token = getToken(authorizationCode);
+        } catch (HttpStatusCodeException e) {
+            response.setStatus(e.getStatusCode().value());
+            return e.getResponseBodyAsString();
         }
 
+        if (token == null) return tokenIsInvalidServerError(response);
+
+        UserModel userModel = UserRestGetter.getUser(token);
+        if (userModel == null) return tokenIsInvalidServerError(response);
+
+        User user = userRepository.findByUsername(userModel.getUser().getUsername()).orElse(null);
+
+        HttpSession oldSession = request.getSession(false);
+        if (oldSession != null) {
+            oldSession.invalidate();
+        }
+        request.getSession(true);
+
+        try {
+            if (user == null) return setNewUser(response, userModel.getUser(), token);
+            else return updateOldUser(response, user, token);
+
+        } catch (IOException e) {
+            response.setStatus(500);
+            return new ErrorMessage("internal_server_error", "Error occurred when trying redirect: " + e.getMessage());
+        }
+
+
     }
+
+    private Token getToken(String code) throws HttpStatusCodeException {
+        return new Request().get("oauth", Token.class, "authorization_code", code).getBody();
+    }
+
+    private ErrorMessage tokenIsInvalidServerError(HttpServletResponse response) {
+        response.setStatus(500);
+        return new ErrorMessage("invalid_token", "Responded invalid tokens from Helios API");
+    }
+
+    private Object setNewUser(HttpServletResponse response, com.apploidxxx.heliosbackend.rest.model.User uninitializedUser, Token token) throws IOException {
+        User user = generateNewUser(token, uninitializedUser);
+        userRepository.save(user);
+        return saveUserSessionAndRedirectToMainPage(response, user);
+    }
+
+    private Object updateOldUser(HttpServletResponse response, User user, Token token) throws IOException {
+        user.setUserToken(token);
+        userRepository.save(user);
+        return saveUserSessionAndRedirectToMainPage(response, user);
+    }
+
+    private User generateNewUser(Token token, com.apploidxxx.heliosbackend.rest.model.User user) {
+
+        return new User(token, SessionGenerator.generateSession(user.getUsername(), user.getFirstName()), user.getUsername());
+
+    }
+
+    private Object saveUserSessionAndRedirectToMainPage(HttpServletResponse response, User user) throws IOException {
+        setSessionCookie(response, user);
+        response.setStatus(HttpServletResponse.SC_OK);
+        return redirectToMainPage(response);
+    }
+
+    private void setSessionCookie(HttpServletResponse response, User user) {
+        Cookie c = new Cookie("session", user.getSession());
+        response.addCookie(c);
+    }
+
+    private Object redirectToMainPage(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/helios.html");
+        return null;
+    }
+
+
 }
