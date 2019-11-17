a80becb2885cbfad4a2b8918354b5cf190061f70
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-11-17 17:54:41 +0300

    added HttpStatusCode Error handling and documented

--- src/main/java/com/apploidxxx/heliosbackend/handlers/exception/ExceptionHandler.java
+++ src/main/java/com/apploidxxx/heliosbackend/handlers/exception/ExceptionHandler.java
@@ -7,16 +7,30 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.client.HttpStatusCodeException;
 
 /**
+ *
+ * Handles some of exceptions caused in runtime
+ *
  * @author Arthur Kupriyanov
  */
 @ControllerAdvice
 public class ExceptionHandler {
+
+    /**
+     * Caught WebException errors which have {@link WebException#getErrorMessage()} method
+     * @param e caused exception
+     * @return response with status code and {@link ErrorMessage}
+     */
     @org.springframework.web.bind.annotation.ExceptionHandler(WebException.class)
     public ResponseEntity<ErrorMessage> userNotFoundExceptionHandle(WebException e) {
         return new ResponseEntity<>(e.getErrorMessage(), e.getStatusCode());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    /**
+     * Caught the Exceptions caused by {@link org.springframework.web.client.RestTemplate}
+     * @param e caused exception
+     * @return response with status code and response body of error
+     */
+    @org.springframework.web.bind.annotation.ExceptionHandler(HttpStatusCodeException.class)
     public ResponseEntity<String> httpStatusCodeException(HttpStatusCodeException e) {
         return new ResponseEntity<>(e.getResponseBodyAsString(), e.getStatusCode());
     }
